6fa3c3c6da3cc94738f8bd289253118315fae359
kshipra-jadav <jadavkshipra2@gmail.com>
2024-04-13 13:16:49 +0530

    change home route

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return render_template("index.html")
+    return "Hello there"
 
 
 @app.route("/getResults", methods=["POST"])
